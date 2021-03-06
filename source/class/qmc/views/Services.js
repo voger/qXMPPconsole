a340c24f1d5e0f161ea6db9c416ef617d58b2605
voger <voger@runbox.com>
2021-03-06 23:35:14 +0200

    Disco works

--- source/class/qmc/views/Services.js
+++ source/class/qmc/views/Services.js
@@ -23,17 +23,24 @@ qx.Class.define("qmc.views.Services", {
     const self = this;
     this.__servicesTree.bind("selection[0].info", this.__servicesDescr, "html", {
       converter(data, model, source, target) {
-        const identities = (data && data.getChildren("identity")) ?? [];
+        // data can be undefined, an xmppjs query element or an error string
+        // make sure we can call get children
+        const canGetChildren = data && qx.lang.Type.isFunctionOrAsyncFunction(data.getChildren);
+        const identities = (canGetChildren && data.getChildren("identity")) ?? [];
         const hasIdentities = identities.length > 0;
 
-        const features = (data && data.getChildren("feature")) ?? [];
+        const features = (canGetChildren && data.getChildren("feature")) ?? [];
         const hasFeatures = features.length > 0;
+
+        // if it is a string assign it to the object
+        const errorString = qx.lang.Type.isString(data) ? data : null;
 
         const view = {
           identities: identities,
           hasIdentities: hasIdentities,
           features: features,
-          hasFeatures: hasFeatures
+          hasFeatures: hasFeatures,
+          errorString: errorString
         };
 
         return q.template.render(self.__template, view);
@@ -54,6 +61,9 @@ qx.Class.define("qmc.views.Services", {
     __servicesDescr: null,
 
     __template: `
+          {{#errorString}}
+            <h3>{{errorString}}</h3>
+          {{/errorString}}
           {{#hasIdentities}}
             <h3>Identities</h3>
             <ul id="identity-list">
@@ -121,7 +131,8 @@ qx.Class.define("qmc.views.Services", {
           children: [],
           icon: "default",
           loaded: false,
-          info: null
+          info: null,
+          node: null
         };
 
         this.__servicesTree.setModel(qx.data.marshal.Json.createModel(root, true));
@@ -134,11 +145,21 @@ qx.Class.define("qmc.views.Services", {
     // creates a tree widget and adds it to the container
     _initTreeWidget() {
       const tree = (this.__servicesTree = new qx.ui.tree.VirtualTree(null, "label", "children", false));
+
+      tree.setLabelOptions({
+        converter(data, model, source, target) {
+          const node = model.getNode();
+          const nodeString = node ? `[${node}]` : "";
+
+          return data + nodeString;
+        }
+      });
+
       tree.setIconPath("icon");
       tree.setIconOptions({
         converter: function (value, model) {
           if (value == "default") {
-            if (model.getChildren != null) {
+            if (model.getChildren().length > 0) {
               return "icon/32/places/network-workgroup.png";
             }
             return "icon/32/places/network-server.png";
@@ -177,11 +198,11 @@ qx.Class.define("qmc.views.Services", {
       tree.setDelegate(delegate);
 
       // prettier-ignore
-      tree.getSelection().addListener( "change", function (e) {
+      tree.getSelection().addListener("change", async function (e) {
           const selected = e.getData().added[0];
           if (selected) {
-            this.sendItemsQuery(selected);
-            this.sendInfoQuery(selected);
+            await this.sendItemsQuery(selected);
+            await this.sendInfoQuery(selected);
           }
         }, this);
 
@@ -196,20 +217,20 @@ qx.Class.define("qmc.views.Services", {
     },
 
     async sendItemsQuery(item) {
-      // item is a leaf. Nothing to query
-      if (item.getChildren == null) {
-        return;
-      }
+      // // item is a leaf. Nothing to query
+      // if (item.getChildren == null) {
+      //   return;
+      // }
 
-      // children are loaded. Nothing to query
-      if (item.getLoaded()) {
-        return;
-      }
+      // // children are loaded. Nothing to query
+      // if (item.getLoaded()) {
+      //   return;
+      // }
 
       try {
         const service = qmc.service.Service.getInstance();
         const from = service.getConnection().jid.toString();
-        const to = service.getDomain();
+        const to = item.getLabel();
         const namespace = this.constructor.discoNS.ITEMS;
         const element = service.xml("iq", {from: from, type: "get", to: to}, service.xml("query", {xmlns: namespace}));
         const response = await service.request(element);
@@ -225,30 +246,50 @@ qx.Class.define("qmc.views.Services", {
         const service = qmc.service.Service.getInstance();
         const from = service.getConnection().jid.toString();
         const to = item.getLabel();
-        const namespace = this.constructor.discoNS.INFO;
-        const element = service.xml("iq", {from: from, type: "get", to: to}, service.xml("query", {xmlns: namespace}));
+
+        const queryObj = {
+          xmlns: this.constructor.discoNS.INFO
+        };
+
+        const node = item.getNode();
+        if (node !== null) {
+          queryObj.node = node;
+        }
+
+        const element = service.xml("iq", {from: from, type: "get", to: to}, service.xml("query", queryObj));
         const response = await service.request(element);
         const info = response.getChild("query");
         item.setInfo(info);
       } catch (err) {
-        console.error(err);
+        item.setInfo(err.toString());
       }
     },
 
     __addNodeChildren(parent, result) {
       for (const item of result) {
-        let node = {
-          label: item.attrs.jid,
-          icon: "default",
-          loaded: false,
-          info: null
-        };
+        const jid = item.attrs.jid;
+        const node = item.attrs.node ?? null;
 
-        if (item.attrs.node !== undefined) {
-          node["children"] = [];
+        // ensure we are pushing unique children
+        const children = parent.getChildren();
+
+        // prettier-ignore
+        const inChildren = children.toArray().find((child) => {
+              return child.getLabel() === jid && 
+              child.getNode() === node;});
+
+        if (!inChildren) {
+          let treeNode = {
+            label: jid,
+            icon: "default",
+            loaded: false,
+            info: null,
+            node: node ?? null,
+            children: []
+          };
+
+          parent.getChildren().push(qx.data.marshal.Json.createModel(treeNode, true));
         }
-
-        parent.getChildren().push(qx.data.marshal.Json.createModel(node, true));
       }
     }
   }
